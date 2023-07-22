c0f3ec523b6e593f65ebb3d2c5a4ae37c4056ee6
Jayson <kanekiken101017@gmail.com>
2023-07-22 18:40:40 +0800

    initial commit

--- app/src/main/java/com/vvv/quiz/QuizUtils.java
+++ app/src/main/java/com/vvv/quiz/QuizUtils.java
@@ -8,99 +8,99 @@ import java.util.List;
 public class QuizUtils {
 
     private static final String[] SET1_QUESTIONS = {
-            "What is the capital of France?",
-            "Which planet is known as the Red Planet?",
-            "What is the largest mammal on Earth?",
-            "Who wrote the play 'Hamlet'?",
-            "What is the chemical symbol for water?"
+            "Which logo is this?",
+            "Which animal is known as the 'King of the Jungle'?",
+            "What is the largest planet in our solar system?",
+            "Which country is famous for the Great Pyramid of Giza?",
+            "What is the main ingredient in guacamole?"
     };
 
     private static final String[][] SET1_CHOICES = {
-            {"Paris", "London", "Berlin", "Rome"},
-            {"Mars", "Venus", "Jupiter", "Saturn"},
-            {"Blue Whale", "African Elephant", "Giraffe", "Hippopotamus"},
-            {"William Shakespeare", "Jane Austen", "Charles Dickens", "Mark Twain"},
-            {"H", "O", "C", "N"}
+            {"Adidas", "Nike", "New Balance", "Converse"},
+            {"Lion", "Elephant", "Tiger", "Giraffe"},
+            {"Jupiter", "Saturn", "Neptune", "Mars"},
+            {"Egypt", "Greece", "Italy", "China"},
+            {"Avocado", "Tomato", "Onion", "Lime"}
     };
 
     private static final int[] SET1_IMAGES = {
-            R.drawable.crown,
-            R.drawable.sad,
-            R.drawable.quiz_ic,
-            R.drawable.quiz_act,
-            R.drawable.info
+            R.drawable.adidas,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable
     };
 
     private static final String[] SET2_QUESTIONS = {
-            "What is the capital of Italy?",
-            "Which planet is known as the Blue Planet?",
-            "What is the tallest animal on Earth?",
-            "Who wrote the play 'Romeo and Juliet'?",
-            "What is the chemical symbol for oxygen?"
+            "Which city is known as the Big Apple?",
+            "Who is known as the 'Father of Modern Physics'?",
+            "What is the chemical symbol for gold?",
+            "Which famous scientist developed the theory of relativity?",
+            "What is this used for?"
     };
 
     private static final String[][] SET2_CHOICES = {
-            {"Rome", "Paris", "Berlin", "London"},
-            {"Earth", "Venus", "Mars", "Mercury"},
-            {"Giraffe", "African Elephant", "Blue Whale", "Hippopotamus"},
-            {"William Shakespeare", "Jane Austen", "Charles Dickens", "Mark Twain"},
-            {"O", "H", "C", "N"}
+            {"New York City", "Los Angeles", "Chicago", "Houston"},
+            {"Albert Einstein", "Isaac Newton", "Galileo Galilei", "Niels Bohr"},
+            {"Au", "Ag", "G", "H2o"},
+            {"Albert Einstein", "Isaac Newton", "Charles Darwin", "Niels Bohr"},
+            {"Writing", "Speaking", "Reading", "Hunting"}
     };
 
     private static final int[] SET2_IMAGES = {
-            R.drawable.back,
-            R.drawable.crown,
-            R.drawable.info,
-            R.drawable.sad,
-            R.drawable.quiz_act
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable,
+            R.drawable.pen
     };
 
     private static final String[] SET3_QUESTIONS = {
-            "What is the capital of Japan?",
-            "Which planet is known as the Ringed Planet?",
-            "What is the fastest land animal?",
-            "Who wrote the play 'Macbeth'?",
-            "What is the chemical symbol for gold?"
+            "Which instrument is known as the 'king of musical instruments'?",
+            "What animal is this?",
+            "What is the currency of Brazil?",
+            "What is the main ingredient in hummus?",
+            "Which river is the longest in the world?"
     };
 
     private static final String[][] SET3_CHOICES = {
-            {"Tokyo", "Beijing", "Seoul", "New Delhi"},
-            {"Saturn", "Mars", "Jupiter", "Uranus"},
-            {"Cheetah", "Lion", "Leopard", "Elephant"},
-            {"William Shakespeare", "Jane Austen", "Charles Dickens", "Mark Twain"},
-            {"Au", "Ag", "Fe", "Pb"}
+            {"Piano", "Violin", "Guitar", "Flute"},
+            {"Dog", "Bird", "Cat", "Person"},
+            {"Real", "Euro", "Peso", "Dollar"},
+            {"Chickpeas", "Lentils", "Eggplant", "Tomato"},
+            {"Nile", "Amazon", "Yangtze", "Mississippi"}
     };
 
     private static final int[] SET3_IMAGES = {
-            R.drawable.quiz_act,
-            R.drawable.crown,
-            R.drawable.sad,
-            R.drawable.back,
-            R.drawable.info
+            R.drawable.empty_drawable,
+            R.drawable.dog,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable
     };
 
     private static final String[] SET4_QUESTIONS = {
-            "What is the capital of Australia?",
-            "Which planet is known as the Morning Star?",
-            "What is the largest bird in the world?",
-            "Who wrote the play 'The Great Gatsby'?",
-            "What is the chemical symbol for silver?"
+            "Which country is known as the 'Land of the Pharaohs'?",
+            "Which country is this flag?",
+            "What is the chemical symbol for iron?",
+            "Select the odd number below.",
+            "What insect is this?"
     };
 
     private static final String[][] SET4_CHOICES = {
-            {"Canberra", "Sydney", "Melbourne", "Perth"},
-            {"Venus", "Mars", "Jupiter", "Mercury"},
-            {"Ostrich", "Penguin", "Eagle", "Albatross"},
-            {"F. Scott Fitzgerald", "Jane Austen", "Charles Dickens", "Mark Twain"},
-            {"Ag", "Au", "Fe", "Pb"}
+            {"Egypt", "Greece", "Rome", "India"},
+            {"Vietnam", "China", "Japan", "Thailand"},
+            {"Fe", "Ag", "Zn", "Au"},
+            {"11", "92", "84", "08"},
+            {"Ant", "Spider", "Frog", "Mosquito"}
     };
 
     private static final int[] SET4_IMAGES = {
-            R.drawable.info,
-            R.drawable.crown,
-            R.drawable.sad,
-            R.drawable.quiz_act,
-            R.drawable.back
+            R.drawable.empty_drawable,
+            R.drawable.vietnam,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable
     };
 
     private static final String[] SET5_QUESTIONS = {
@@ -108,7 +108,7 @@ public class QuizUtils {
             "Which planet is known as the Evening Star?",
             "What is the largest fish in the ocean?",
             "Who wrote the novel 'To Kill a Mockingbird'?",
-            "What is the chemical symbol for carbon?"
+            "What is this called?"
     };
 
     private static final String[][] SET5_CHOICES = {
@@ -116,18 +116,18 @@ public class QuizUtils {
             {"Venus", "Mars", "Jupiter", "Mercury"},
             {"Whale Shark", "Blue Whale", "Great White Shark", "Giant Squid"},
             {"Harper Lee", "Jane Austen", "Charles Dickens", "Mark Twain"},
-            {"C", "O", "H", "N"}
+            {"Moon", "Sun", "Sky", "Star"}
     };
 
     private static final int[] SET5_IMAGES = {
-            R.drawable.quiz_ic,
-            R.drawable.crown,
-            R.drawable.back,
-            R.drawable.sad,
-            R.drawable.info
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable,
+            R.drawable.empty_drawable,
+            R.drawable.moon
     };
 
-    private static final boolean[] HAS_IMAGE_ARRAY = {false, false, false, false, false, true};
+    private static final boolean[] HAS_IMAGE_ARRAY = {true, true, true, true, true};
 
     public static List<Question> generateRandomQuestions(int numQuestions, int set) {
         List<Question> questions = new ArrayList<>();
